e3dda99149b867e454643388f6210959f96ef561
= <seoheewon123@naver.com>
2024-08-01 22:01:57 +0900

    fix: form linkButton src

--- app/(user)/form/linkButton.tsx
+++ app/(user)/form/linkButton.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function LinkButton() {
     return (
         <Button type="submit" className="w-96 h-12  mt-72 ml-5 rounded-xl">
-            <Link href="/form">시작하기</Link>
+            <Link href="/list">시작하기</Link>
         </Button>
     )
 }

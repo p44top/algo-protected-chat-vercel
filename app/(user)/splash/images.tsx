6449c38155c4040f1fd6330afafb4301d8a05729
= <seoheewon123@naver.com>
2024-08-01 22:01:32 +0900

    fix: splash

---
+++ app/(user)/splash/images.tsx
@@ -0,0 +1,17 @@
+export const images = [
+    {
+        src: 'webp/firstImage.webp',
+        f_description: '실제 사례를 학습하여',
+        s_description: '피싱 피해를 방지해요',
+    },
+    {
+        src: 'webp/secondImage.webp',
+        f_description: 'AI로 실전 피싱 대응을',
+        s_description: '연습할 수 있어요',
+    },
+    {
+        src: 'webp/thirdImage.webp',
+        f_description: 'AI 피드백으로 피싱',
+        s_description: '대응 능력을 향상시켜요',
+    },
+]
